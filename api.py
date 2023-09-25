1ab10055ce7b460a2708158c76fac259b9fbcad5
kremastra <fotisgrek@gmail.com>
2023-09-25 15:26:05 +0300

    deleted for gitignore

--- api.py
+++
@@ -1,7 +0,0 @@
-from telebot import TeleBot
-
-bot = TeleBot("6077065506:AAE4XLoS0Tb0YF2_lc5dpL39vcZtsxj_0xA", parse_mode=None)
-
-#from telebot.async_telebot import AsyncTeleBot
-
-#bot = AsyncTeleBot("6077065506:AAE4XLoS0Tb0YF2_lc5dpL39vcZtsxj_0xA", parse_mode=None)
