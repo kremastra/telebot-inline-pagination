43fe2533f3de27977fcb39ec3941c2f106a74aa8
Mikias <mikias_abiy@proton.me>
2024-04-05 16:48:01 +0300

    update: __init__.py: New feature row_width handling implemented.

--- telebot_inline_pagination/__init__.py
+++ telebot_inline_pagination/__init__.py
@@ -1,22 +1,51 @@
-"""Pageable inline keyboard for pyTelegramBotAPI.
+#!/usr/bin/python3
 
-A library for pyTelegramBotAPI that allows you to display data in the format of a pageable inline keyboard.
+"""
+inline_pagination.py:
+    This module contains difination of the class Keyboard
 """
 
 from math import ceil
-from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup,\
+    CallbackQuery
+
 
 class Keyboard():
-    """Pageable inline keyboard which contain inline buttons."""
+    """
+    Keyboard:
+        A class that handles InlineKeyboardMarkup (reply keyboard markup in
+        Telegram bot API) buttons pagination.
 
-    def __init__(self, chat_id: int, data: list, rows_per_page=5, button_text_mode=0, text_index=0, callback_index=0, next_page='--->', previous_page='<---'):
-        """Construct a pageable inline keyboard."""        
+    This is a documentation for the __init__ magic function of the class.
+    Args:
+        chat_id (int): The id of the chat that the message containg the inline
+                       keyboard was sent in.
+        data (List[Tuple]): List of tuples containing ('button test',
+                            'call back data') in this format.
+        row_width (int): Number of buttons to show per row.
+        rows_per_page (int): Number of rows to show per page.
+        button_text_mode (int): The mode which the buttons texts will be
+                                displayed.
+        text_index (int): The index of the button text in the tuples in
+                          the data list you provided.
+        callback_index (int): The index of the callback data in the the tuples
+                              in the data list you provided.
+        next_page (str): The content that'll be shown in the next_page button.
+        previous_page (str): The content that'll be shown in the previous_page
+                             button.
+    """
+
+    def __init__(
+        self, chat_id, data, row_width=1, rows_per_page=5, button_text_mode=0,
+        text_index=0, callback_index=0, next_page='--->', previous_page='<---'
+    ):
         self.chat_id = chat_id
         self.current_page = 0
         self.data = data
-        self.count = len(self.data)
+        self.count = len(self.data) / row_width
         self.pages = ceil(self.count/rows_per_page)
         self.rows_per_page = rows_per_page
+        self.row_width = row_width
         self.button_text_mode = button_text_mode
         self.text_index = text_index
         self.callback_index = callback_index
@@ -24,33 +53,101 @@ class Keyboard():
         self.previous_page = previous_page
 
     def text_callback(self):
-        keyboard = InlineKeyboardMarkup()
-        for i in self.data[self.current_page*self.rows_per_page: self.current_page*self.rows_per_page+self.rows_per_page]:
-            if self.button_text_mode == 1:
-                button = InlineKeyboardButton(text=i[self.text_index], callback_data=i[self.callback_index])    
-            elif self.button_text_mode == 2:
-                button = InlineKeyboardButton(text=i[self.text_index] + ' (' + i[self.callback_index] + ')', callback_data=i[self.callback_index])               
-            elif self.button_text_mode == 3:
-                button = InlineKeyboardButton(text=i[self.callback_index] + ' (' + i[self.text_index] + ')', callback_data=i[self.callback_index])   
-            else:
-                button = InlineKeyboardButton(text=i[0], callback_data=i[0])
-            keyboard.add(button)
+        """
+        text_callback: Generates the buttons for a single page. Determins
+                       content based on the self.current_page variable.
+
+        Return:
+            InlineKeyboardMarkup: object containg the button for the
+                                  self.current_page.
+        """
+        keyboard = InlineKeyboardMarkup(row_width=self.row_width)
+        btns_per_page = self.rows_per_page * self.row_width
+        page_data = self.data[
+                self.current_page * btns_per_page:
+                self.current_page * btns_per_page + btns_per_page
+            ]
+
+        try:
+            for i in range(0, len(page_data), self.row_width):
+                button = []
+                if self.button_text_mode == 1:
+                    for j in range(0, self.row_width):
+                        button.append(
+                            InlineKeyboardButton(
+                                text=page_data[i + j][self.text_index],
+                                callback_data=page_data[i + j][self.callback_index]
+                            )
+                        )
+                elif self.button_text_mode == 2:
+                    for j in range(0, self.row_width):
+                        button.append(
+                            InlineKeyboardButton(
+                                text=page_data[i + j][self.text_index] + ' (' + i[self.callback_index] + ')',
+                                callback_data=page_data[i + j][self.callback_index]
+                            )
+                        )
+                elif self.button_text_mode == 3:
+                    for j in range(0, self.row_width):
+                        button.append(
+                            InlineKeyboardButton(
+                                text=page_data[i + j][self.callback_index] + ' (' + i[self.text_index] + ')',
+                                callback_data=page_data[i + j][self.callback_index]
+                            )
+                        )
+                else:
+                    for j in range(0, self.row_width):
+                        button.append(
+                            InlineKeyboardButton(
+                                text=page_data[i + j][0],
+                                callback_data=page_data[i + j][0]
+                            )
+                        )
+                keyboard.row(*button)
+        except IndexError as e:
+            keyboard.row(*button)
+            pass
         return keyboard
 
     def send_keyboard(self):
-        """Return first page of the inline keyboard."""       
-        keyboard = self.text_callback()      
+        """
+        send_keyboard: Generates the first page of the keyboard.
+
+        Return:
+            InlineKeyboardMarkup: object containg the button for the
+            self.current_page and navigation buttons if there are
+            multiple pages.
+        """
+        keyboard = self.text_callback()
+
         if self.pages > 1:
             keyboard.add(
                     InlineKeyboardButton(text='-', callback_data='-'),
-                    InlineKeyboardButton(text=f'{1}/{self.pages}', callback_data=f'{1}/{self.pages}'),
-                    InlineKeyboardButton(text=self.next_page, callback_data='next_page')
+                    InlineKeyboardButton(
+                        text=f'{1}/{self.pages}',
+                        callback_data=f'{1}/{self.pages}'
+                    ),
+                    InlineKeyboardButton(
+                        text=self.next_page,
+                        callback_data='next_page'
                     )
+                )
         return keyboard
 
     def edit_keyboard(self, call: CallbackQuery):
-        """Navigate between pages using the 'next page' and 'previous page' buttons."""        
-        if call.data == 'next_page' and self.current_page < self.pages:       
+        """
+        edit_keyboard: Edits keyboard pages when call from next_page and
+                       previous_page comes in.
+
+        Args:
+            call (CallbackQuery :obj:): The CallbackQuery object from the
+                                        next_page or previous_page buttons.
+
+        Return:
+            InlineKeyboardMarkup: object containg the button for the
+            self.current_page and navigation buttons.
+        """
+        if call.data == 'next_page' and self.current_page < self.pages:
             self.current_page = self.current_page + 1
         elif call.data == 'previous_page' and self.current_page > 0:
             self.current_page = self.current_page - 1
@@ -60,20 +157,37 @@ class Keyboard():
         if self.current_page == 0:
             keyboard.add(
                 InlineKeyboardButton(text='-', callback_data='-'),
-                InlineKeyboardButton(text=f'{self.current_page+1}/{self.pages}', callback_data=f'{self.current_page+1}/{self.pages}'),
-                InlineKeyboardButton(text=self.next_page, callback_data='next_page')
+                InlineKeyboardButton(
+                    text=f'{self.current_page+1}/{self.pages}',
+                    callback_data=f'{self.current_page+1}/{self.pages}'
+                ),
+                InlineKeyboardButton(
+                    text=self.next_page, callback_data='next_page'
                 )
+            )
         elif self.pages - self.current_page == 1:
             keyboard.add(
-                InlineKeyboardButton(text=self.previous_page, callback_data='previous_page'),
-                InlineKeyboardButton(text=f'{self.current_page+1}/{self.pages}', callback_data=f'{self.current_page+1}/{self.pages}'),
+                InlineKeyboardButton(
+                    text=self.previous_page, callback_data='previous_page'
+                ),
+                InlineKeyboardButton(
+                    text=f'{self.current_page+1}/{self.pages}',
+                    callback_data=f'{self.current_page+1}/{self.pages}'
+                ),
                 InlineKeyboardButton(text='-', callback_data='-')
-                )            
+            )
         else:
             keyboard.add(
-                InlineKeyboardButton(text=self.previous_page, callback_data='previous_page'),
-                InlineKeyboardButton(text=f'{self.current_page+1}/{self.pages}', callback_data=f'{self.current_page+1}/{self.pages}'),
-                InlineKeyboardButton(text=self.next_page, callback_data='next_page')
-                )      
-        return keyboard      
-   
+                InlineKeyboardButton(
+                    text=self.previous_page, callback_data='previous_page'
+                ),
+                InlineKeyboardButton(
+                    text=f'{self.current_page+1}/{self.pages}',
+                    callback_data=f'{self.current_page+1}/{self.pages}'
+                ),
+                InlineKeyboardButton(
+                    text=self.next_page, callback_data='next_page'
+                )
+            )
+
+        return keyboard
